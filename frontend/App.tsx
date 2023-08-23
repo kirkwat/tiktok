53d05c4e74f817492fea9f0acfefe4961d915653
kirkwat <watson.kirk00@gmail.com>
2023-08-22 23:35:32 -0500

    build: add redux

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Provider, useDispatch, useSelector } from "react-redux";
+
+import { store } from "./src/store/store";
+import { increment, decrement } from "./src/store/slices/counterSlice";
 
 export default function App() {
   return (
+    <Provider store={store}>
+      <CounterComponent />
+    </Provider>
+  );
+}
+
+function CounterComponent() {
+  const dispatch = useDispatch();
+
+  // Replace 'counter' with whatever key you used in your rootReducer
+  const count = useSelector((state) => state.counter.value);
+
+  return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>{`Count: ${count}`}</Text>
+      <Button title="Increment" onPress={() => dispatch(increment())} />
+      <Button title="Decrement" onPress={() => dispatch(decrement())} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ frontend/src/store/slices/counterSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+
+export default counterSlice.reducer;
