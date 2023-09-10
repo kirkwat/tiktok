deca1f0b0e781de6ac73f54f7017f05754ece4bc
kirkwat <watson.kirk00@gmail.com>
2023-09-10 18:12:01 -0500

    chore: default firebase values

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -3,23 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
-
-// Initialize Firebase
+//! REPLACE VALUES BELOW WITH YOUR OWN FIREBASE CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyCE4Dn4dGOhwcpYyVsUUTAtMEOCNfJlJKo",
-  authDomain: "ate-out-of-10.firebaseapp.com",
-  databaseURL: "https://ate-out-of-10-default-rtdb.firebaseio.com",
-  projectId: "ate-out-of-10",
-  storageBucket: "ate-out-of-10.appspot.com",
-  messagingSenderId: "899851871046",
-  appId: "1:899851871046:web:b894ea5825f06bb588fb47",
-  measurementId: "G-844MXPVSG4",
+  apiKey: "*****",
+  authDomain: "*****",
+  databaseURL: "*****",
+  projectId: "*****",
+  storageBucket: "*****",
+  messagingSenderId: "*****",
+  appId: "*****",
+  measurementId: "*****",
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
